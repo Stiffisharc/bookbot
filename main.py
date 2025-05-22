62125fb026c34aa98b84aba8ec5bf8cc2d173a3f
Stiffisharc <StiffisharcGitHub@pm.me>
2025-05-21 21:08:10 -0700

    Project complete

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import count_words
 from stats import count_letters
 from stats import sort_letters
@@ -8,22 +9,38 @@ def get_book_text(path_to_book):
         book_text = f.read()
         return book_text
 
-def main():
-    book_path = "books/frankenstein.txt"
-    book_text = get_book_text(book_path)
-    word_count = count_words(book_text)
-    counted_letters = count_letters(book_text)
-    listed_letters = list_letters(counted_letters)
-    listed_letters.sort(reverse=True, key=sort_letters)
+def print_report(book_path, word_count, letter_list):
     print(" ")
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
     print(f"Found {word_count} total words")
     print("--------- Character Count -------")
-    for letter in listed_letters:
+    for letter in letter_list:
         print(f"{letter["letter"]}: {letter["count"]}")
     print("============= END ===============")
     print(" ")
+
+def main():
+    err = "Usage: python3 main.py <path_to_book>"
+    try:
+        if sys.argv[1] == None:
+            trigger = None
+    except IndexError:
+        print(err)
+        sys.exit(1)
+    try:
+        if sys.argv[2] != None:
+            print(err)
+            sys.exit(1) 
+    except IndexError:
+        trigger = None
+    book_path = sys.argv[1]
+    book_text = get_book_text(book_path)
+    word_count = count_words(book_text)
+    counted_letters = count_letters(book_text)
+    listed_letters = list_letters(counted_letters)
+    listed_letters.sort(reverse=True, key=sort_letters)
+    print_report(book_path, word_count, listed_letters)
 
 main()
