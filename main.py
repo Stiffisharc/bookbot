2ad42fa54de3cb8d297a06072f9b27bc66daa105
Stiffisharc <StiffisharcGitHub@pm.me>
2025-05-21 17:48:47 -0700

    Label removal in progress. Pushing to ask for help.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from stats import count_words
 from stats import count_letters
 from stats import sort_letters
 from stats import list_letters
+from stats import rem_labels
 
 def get_book_text(path_to_book):
     with open(path_to_book) as f:
@@ -15,13 +16,14 @@ def main():
     counted_letters = count_letters(book_text)
     listed_letters = list_letters(counted_letters)
     listed_letters.sort(reverse=True, key=sort_letters)
+    letters = rem_labels(listed_letters)
     print(" ")
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
     print(f"Found {word_count} total words")
     print("--------- Character Count -------")
-    for letter in listed_letters:
+    for letter in letters:
         print(letter)
     print("============= END ===============")
     print(" ")
